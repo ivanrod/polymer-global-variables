2cb80558338c1af8a737cba7a0a236b235f2aa46
Iván Rodriguez <ivan.rodriguez@bq.com>
2016-10-22 21:24:59 +0200

    Add code coments

--- index.js
+++ index.js
@@ -20,6 +20,8 @@
         set: function(key, value) {
             this.globals[key] = value;
 
+            // Iterate through every element created
+            // and use 'set' to notify changes in all of them
             for (var i in this.elementsInstances) {
                 this.elementsInstances[i].set('globals.' + key, value);
             }
@@ -29,9 +31,16 @@
     };
 
     Polymer.Base._addFeature({
+        // Replace _configureProperties method to load elements properties with globals property
         _configureProperties: function(properties, config) {
-            Polymer.globalsManager.elementsInstances.push(this);
+            var instances = Polymer.globalsManager.elementsInstances;
 
+            // Prevent duplicate instances
+            if (instances.indexOf(this) < 0) {
+                Polymer.globalsManager.elementsInstances.push(this);
+            }
+
+            // Add globals property to every instance
             if (properties) {
                 properties.globals = {
                     type: Object,
@@ -39,6 +48,7 @@
                 };
             }
 
+            // Continue using the original _configureProperties method
             __configureProperties.apply(this, [properties, config]);
         }
     });
